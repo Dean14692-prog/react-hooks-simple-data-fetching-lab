18387b2e5286f6913c079f83191df14fdd662fa5
Dean14692-prog <dennismuasyangui@gmail.com>
2025-04-22 16:47:42 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-
-import "./index.css";
+import App from "/home/dean/Moringa/Phase2/react-hooks-simple-data-fetching-lab/src/components/App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
